2d6d5197ee0a629a526354f7bd08627fa47f733b
John Berberich <jberberich@gmail.com>
2013-08-01 10:30:26 -0400

    Exercise 34

---
+++ 34.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# http://ruby.learncodethehardway.org/book/ex34.html
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts animals[1]
+
+puts animals[2]
+
+puts animals[0]
+
+puts animals[3]
+
+puts animals[4]
+
+puts animals[2]
+
+puts animals[5]
+
+puts animals[4]
+
